eb478a44f8d0bc4f39d40f872ccbcd49272b88f5
rqureshi <rqureshi@rqureshisp3.bayviewglen.ca>
2017-05-04 19:19:00 -0400

    oushed

--- Zork/src/com/bayviewglen/zork/Game.java
+++ Zork/src/com/bayviewglen/zork/Game.java
@@ -131,7 +131,6 @@ class Game
     {
         System.out.println();
         System.out.println("Welcome to Zork!");
-        System.out.println("Zork is a new, incredibly boring adventure game.");
         System.out.println("Type 'help' if you need help.");
         System.out.println();
         System.out.println(currentRoom.longDescription());
@@ -177,9 +176,9 @@ class Game
     private void printHelp() 
     {
         System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at Monash Uni, Peninsula Campus.");
         System.out.println();
         System.out.println("Your command words are:");
+        System.out.println("Type 'go' and then your desired direction");
         parser.showCommands();
     }
 
