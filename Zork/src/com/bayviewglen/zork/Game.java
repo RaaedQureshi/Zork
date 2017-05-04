c82f93597fc75d89e3352634ea91ad0b1ffd3c6d
rqureshi <rqureshi@rqureshisp3.bayviewglen.ca>
2017-05-04 15:29:10 -0400

    push

--- Zork/src/com/bayviewglen/zork/Game.java
+++ Zork/src/com/bayviewglen/zork/Game.java
@@ -95,7 +95,7 @@ class Game
     public Game() {
         try {
 			initRooms("data/Rooms.dat");
-			currentRoom = masterRoomMap.get("ROOM_1");
+			currentRoom = masterRoomMap.get("TOWN_SQUARE");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
