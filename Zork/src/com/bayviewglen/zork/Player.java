4f9d526e2bdcd32a29965654efc9bf88dd74d00c
rqureshi <rqureshi@rqureshisp3.bayviewglen.ca>
2017-05-20 17:49:26 -0400

    fixed player class

--- Zork/src/com/bayviewglen/zork/Player.java
+++ Zork/src/com/bayviewglen/zork/Player.java
@@ -1,9 +1,24 @@
 package com.bayviewglen.zork;
 
-public class Player {
+public abstract class Player {
 	
 	private String name = Parser.playerName;
 	private int playerHealth;
+	private int strength = 0;
+	private int vitality = 0;
+	private int intellect = 0; 
+	private int luck = 0;
+	
+	public Player(int strength, int vitality, int intellect, int luck){
+		strength = strength;
+		vitality = vitality;
+		intellect = intellect;
+		luck = luck;
+		
+		
+	}
+	
+	
 	
 	private static void pressAnyKeyToContinue()
     { 
@@ -41,66 +56,6 @@ public class Player {
 			
 		} else{                          // Character is good
 			
-			System.out.println("In an obscure village lying in the forgotten plains, a child was born");
-			pressAnyKeyToContinue();
-			System.out.println("His Name");
-			pressAnyKeyToContinue();
-			System.out.println(Parser.playerName);
-			pressAnyKeyToContinue();
-			System.out.println("The village was, and his family was poor, but they were happy");
-			pressAnyKeyToContinue();
-			System.out.println("Him, his father, his mother, and little sister loved together "
-					+ "with a few other families in peace");
-			pressAnyKeyToContinue();
-			System.out.println("Years passed as the child lived a normal life and grew up");
-			pressAnyKeyToContinue();
-			System.out.println("But everything changed when the empire attacked...");
-			pressAnyKeyToContinue();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println();
-			System.out.println("You awake to your mother furiously shaking you");
-			pressAnyKeyToContinue();
-			System.out.println("Mother: You need to run");
-			pressAnyKeyToContinue();
-			System.out.println("Mother: Now!");
-			pressAnyKeyToContinue();
-			System.out.println("You hear the sound of screams and horses outside");
-			pressAnyKeyToContinue();
-			System.out.println("She leads you to the back door");
-			pressAnyKeyToContinue();
-			System.out.println("Mother: Go to the temple as fast as you can, hide until they leave");
-			pressAnyKeyToContinue();
-			System.out.println("Mother: Go!");
-			pressAnyKeyToContinue();
-			System.out.println("She runs off");
-			pressAnyKeyToContinue();
-			System.out.println("You make your way to the temple");
-			pressAnyKeyToContinue();
-			System.out.println();
-			System.out.println();
-			System.out.println("...");
-			System.out.println();
-			System.out.println();
-			pressAnyKeyToContinue();
-			System.out.println("Hours have passed since you began hiding");
-			pressAnyKeyToContinue();
-			System.out.println("The soldiers have set up camp in the village for the night "
-					+ "and you cannot get past them until they leave");
-			pressAnyKeyToContinue();
-			System.out.println("Suddenly you hear a noise in the distance");
-			pressAnyKeyToContinue();
-			System.out.println("Footsteps!");
-			pressAnyKeyToContinue();
-			System.out.println("The soldiers have found you, they come closer");
-			pressAnyKeyToContinue();
-			System.out.println("You look around, there is nowehere to hide");
-			pressAnyKeyToContinue();
-			System.out.println("Fat Soldier: Well what do we have here");
-			pressAnyKeyToContinue();
 		}
 	}
 }
